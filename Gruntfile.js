5912851dab0289007eb0362368e77ce94e9289b5
StenW <sten.e.winroth@gmail.com>
2015-01-12 13:21:17 +0100

    removed grunt task to remove standard bower_components forlder and moved task to bower-installer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,18 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		dev: {
 		sass: {
 			dist: {
 				options: {
 					style: "expanded"
 				},
 				files: {
-					"app/assets/css/main.css": "app/app/assets/scss/main.scss"
+					"app/assets/css/main.css": "app/assets/scss/main.scss"
 				}
 			}
-		}
-	},
-	//remove standard bower_components folder and files, in favor of the one specified in the bower-installer	
-	clean: {
-			bower_components: ["bower_components"]
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 
-	grunt.registerTask('default', ['dev']);
+	grunt.registerTask('default', ['sass']);
 };
